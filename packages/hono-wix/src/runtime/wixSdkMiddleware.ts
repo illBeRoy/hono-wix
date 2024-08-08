3b525754d8cd7ba0a06881a3f418af73c3d99298
Roy <roy@sommer.co.il>
2024-08-08 13:47:44 +0300

    support wix context

--- packages/hono-wix/src/runtime/wixSdkMiddleware.ts
+++ packages/hono-wix/src/runtime/wixSdkMiddleware.ts
@@ -1,38 +1,46 @@
 import { AsyncLocalStorage } from 'node:async_hooks';
 import { createMiddleware } from 'hono/factory';
 import { createClient } from '@wix/sdk';
+import { setGlobalWixContext } from '@wix/sdk/context';
 
 const authStorage = new AsyncLocalStorage<{ auth?: string }>();
 
 export const wixSdk = () => {
-  const wixClient = createClient({
-    auth: {
-      getAuthHeaders() {
-        const data = authStorage.getStore();
+  setGlobalWixContext(
+    {
+      initWixModules(wixModules, _elevated) { //eslint-disable-line
+        const wixClient = createClient({
+          auth: {
+            getAuthHeaders() {
+              const data = authStorage.getStore();
 
-        if (!data) {
-          throw new Error(
-            'Please use Wix SDK within a request context to identify the requester.\n' +
-              'If running independently, consider using "elevate" with caution, as it grants admin access.',
-          );
-        }
+              if (!data) {
+                throw new Error(
+                  'Please use Wix SDK within a request context to identify the requester.\n' +
+                    'If running independently, consider using "elevate" with caution, as it grants admin access.',
+                );
+              }
 
-        const { auth } = data;
+              const { auth } = data;
 
-        if (auth) {
-          return {
-            headers: {
-              Authorization: auth,
+              if (auth) {
+                return {
+                  headers: {
+                    Authorization: auth,
+                  },
+                };
+              } else {
+                return { headers: {} as Record<string, string> };
+              }
             },
-          };
-        } else {
-          return { headers: {} as Record<string, string> };
-        }
+          },
+        });
+
+        return wixClient.use(wixModules) as any; //eslint-disable-line
       },
     },
-  });
-
-  wixClient.enableContext('global');
+    { dangerouslyOverride: true },
+  );
 
   return createMiddleware(async (c, next) => {
     const auth = c.req.header('authorization');
